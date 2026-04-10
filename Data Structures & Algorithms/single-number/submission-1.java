dc5b09e90e3290bcba471e88b18cc7d9e2269e8b
Raghav Dabra <66009953+RaghavDabra@users.noreply.github.com>
2026-04-10 22:19:23 +1000

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+    int i =0;
+        while(i<nums.length-1)
+        {
+            if(nums[i] == nums[i+1]){
+            i++;
+            i++;
+            }
+            else{
+            return nums[i];
+         }
+        
+        }
+ return nums[i];
+        }    }
+
