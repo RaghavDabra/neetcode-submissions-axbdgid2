470ca4e038729779ae2e5f7152dec0bfb426f0ba
Raghav Dabra <66009953+RaghavDabra@users.noreply.github.com>
2026-04-21 21:59:35 +1000

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        //I get the piles as the flows ->
+        int left =0;
+        int right =  Arrays.stream(piles).max().getAsInt();
+        int result = right;
+        while(left<=right)
+        {
+            int k = (left+right)/2;
+      long totalTime =0;
+       
+        for(int p : piles)
+        {
+          totalTime += Math.ceil((double) p/k);
+        }
+        if(totalTime <= h)
+        {
+           result = k;
+           right = k-1;
+        }
+        else{
+       left = k+1;
+            
+        
+        }
+        }
+        return result;
+    }
+}
