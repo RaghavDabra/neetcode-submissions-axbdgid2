59424b2f85cca276ca982646b4c36bb02ecfd730
Raghav Dabra <66009953+RaghavDabra@users.noreply.github.com>
2026-05-16 15:07:06 +1000

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int jump(int[] nums) {
+        //bfs approach
+        int left = 0;
+        int right = 0;
+        int result = 0;
+
+        while(right < nums.length-1)
+        {
+            int farthest = 0;
+
+          for(int i = left; i<= right; i++)
+            {
+                farthest = Math.max(farthest, nums[i] + i);
+            }
+            left = right+1;
+            right = farthest;
+            result ++;
+        }
+        return result;
+    }
+}
